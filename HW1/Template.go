09a7b7573457efa2b2422f92ef59b81bff5dbd69
Larry Meyers <ljmeyers80529@mail.fresnostate.edu>
2016-01-26 20:10:41 -0800

    Homework 1 updated

--- HW1/Template.go
+++ HW1/Template.go
@@ -5,13 +5,32 @@
 
 package main
 
-import "fmt" "os" "text/template"
+import (
+        "fmt" 
+        "os" 
+        "text/template"
+       )
 
-type person struct
-{
+type person struct {
+    Name string
+    Age uint8
+    IQ uint8
+    Genius bool
 }
 
 func main() {
-
-
+    p1 := person {
+            Name : "Todd McLeod",
+            Age : 20,
+            IQ: 155,
+    }
+    p1.Genius = p1.IQ >= 150
+    tpl, err := template.ParseFiles("tpl_html_image.html")
+    if err != nil {
+        fmt.Println(err)
+    }
+    err = tpl.Execute(os.Stdout, p1)
+    if err != nil {
+        fmt.Println(err)
+    }
 }
