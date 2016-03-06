9ac53debac78bbbca62211fc58db278c2b2e473a
ljmeyers80529 <ljmeyers80529@mail.fresnostate.edu>
2016-03-06 15:49:20 -0800

    Added web homework assignement #3.

---
+++ WebHW3/main.go
@@ -0,0 +1,34 @@
+// web page homework #3
+// larry meyere
+// 06-Mar-2016
+
+package main
+
+import (
+		"net/http"
+		"io"
+		"fmt"
+		)
+
+var hdr string = "Content-Type"
+var cont string = "text/html; charset=utf-8"
+var body string = `<form method="POST">
+					<input type="text" name="n">
+					<input type="Submit">
+					</form>`
+
+func main() {
+	http.HandleFunc("/", root)
+	http.ListenAndServe(":8080", nil)
+}
+
+func root(res http.ResponseWriter, req *http.Request) {
+	if req.URL.Path != "/" {
+		http.NotFound(res, req)
+	}
+	res.Header().Set(hdr, cont)
+	key := "n"
+	value := req.FormValue(key)
+	fmt.Println("Returned value = "+value)
+	io.WriteString(res, body)
+}
