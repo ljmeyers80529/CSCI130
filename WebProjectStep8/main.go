3e94fec9dff7d12d9bbd3184416d8d13c8759118
ljmeyers80529 <ljmeyers80529@mail.fresnostate.edu>
2016-03-26 20:56:24 -0700

    Allows user to login, but must explictly add the /login to the URL. Step 8 Completed.

---
+++ WebProjectStep8/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+    "net/http"
+    "html/template"
+    "strings"
+    )
+
+var tpl *template.Template
+
+func init() {
+    tpl = template.Must(template.ParseGlob("htmlFiles/*.html"))
+}
+
+func main() {
+    http.Handle("/favicon.ico", http.NotFoundHandler())
+    http.HandleFunc("/", rootPage)
+    http.HandleFunc("/login", loginPage)
+    http.HandleFunc("/logout", logoutPage)
+    http.ListenAndServe(":8080", nil)
+}
+
+// process root / top page.
+func rootPage(res http.ResponseWriter, req *http.Request) {
+    sessionCookie := retrieveCookie(res, req)
+    if req.Method == "POST" {
+        updateCookie(req, sessionCookie)
+    }
+    http.SetCookie(res, sessionCookie)
+    
+    var cookieState = "False"
+    if isCookieInformationValid(sessionCookie) {
+        cookieState = "True"
+    }
+    cookieInfo := cookieInformation {
+        State: cookieState,
+        OriginalCookieValue: sessionCookie.Value,
+        OriginalData: decodeUserInformation(strings.Split(sessionCookie.Value, "|")[1]),
+    }
+    tpl.ExecuteTemplate(res, "root.html", cookieInfo)
+}
+
+func loginPage(res http.ResponseWriter, req *http.Request) {
+    sessionCookie := retrieveCookie(res, req)
+    if req.Method == "POST" && req.FormValue("password") == "csci130" {
+        userInfo := decodeUserInformation(strings.Split(sessionCookie.Value, "|")[1])
+        userInfo.Name = req.FormValue("username")
+        userInfo.LoggedIn = true
+        updateCookieValue(sessionCookie, userInfo)
+        http.SetCookie(res, sessionCookie)
+        http.Redirect(res, req, "/", http.StatusFound)
+        return
+    }
+    tpl.ExecuteTemplate(res, "login.html", nil)
+}
+
+func logoutPage(res http.ResponseWriter, req *http.Request) {
+    sessionCookie := retrieveCookie(res, req)
+    userInfo := decodeUserInformation(strings.Split(sessionCookie.Value, "|")[1])
+    userInfo.LoggedIn = false
+    updateCookieValue(sessionCookie, userInfo)
+    http.SetCookie(res, sessionCookie)
+    http.Redirect(res, req, "/login", http.StatusFound)
+}    
